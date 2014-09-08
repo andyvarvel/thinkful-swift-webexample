cdcefb6b7fa7c83444070b0e5ab5851d6894cd12
Andrew Varvel <andy@fastmail.com.au>
2014-09-08 22:15:57 +1000

    Initial commit

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+
+    @IBOutlet weak var myNameLabel: UILabel!
                             
+    @IBOutlet weak var rainbowWheel: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPRequestOperationManager()
+        rainbowWheel.hidden = false
+        rainbowWheel.hidesWhenStopped = true
+        rainbowWheel.startAnimating()
+ 
+        manager.GET( "http:/graph.facebook.com/andrewv",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                self.rainbowWheel.stopAnimating()
+                println("JSON: " + responseObject.description)
+                if let myName = responseObject.valueForKey("name") as? String {
+                    self.myNameLabel.text = myName;
+                }
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                self.rainbowWheel.stopAnimating()
+
+                println("Error: " + error.localizedDescription)
+        })
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,12 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by ANDREW VARVEL on 8/09/2014.
+//  Copyright (c) 2014 Andrew Varvel. All rights reserved.
+//
+
+
+
+#import "AFNetworking.h"
+
